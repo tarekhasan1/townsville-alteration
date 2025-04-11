df2230c2986eb46b2eb6f4241c0d10e3fd0cdd2e
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2025-04-11 10:41:17 +0600

    env shifted

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,12 +7,12 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBmNuTIbP6sU7bzpytj5Ry2BpugsDOg-ck",
-  authDomain: "tsvalterationsandformalwear.firebaseapp.com",
-  projectId: "tsvalterationsandformalwear",
-  storageBucket: "tsvalterationsandformalwear.firebasestorage.app",
-  messagingSenderId: "701557124789",
-  appId: "1:701557124789:web:a61a0c3f7185518edc58fc"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
