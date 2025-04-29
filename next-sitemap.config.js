ff339fb1f7a8335fd3de203a044eb5421ef61cea
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2025-04-30 00:32:30 +0600

    next sitemap updated

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://www.tsvalterationsandformalwear.com', // Replace with your actual domain
+    generateRobotsTxt: true,           // Enables generating robots.txt
+    sitemapSize: 5000,                 // Optional: control the number of URLs per sitemap
+    outDir: './public',                // Optional: output directory (default: /public)
+  };
+  
--- next-sitemap.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-// next-sitemap.js
-
-const config = require('./next-sitemap.config.ts');
-
-module.exports = config;
